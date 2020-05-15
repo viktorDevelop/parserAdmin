0c3612dc142364ccb05d5f02e0112311af6f3494
viktor <vk.job.00@mail.ru>
2020-05-15 11:54:48 +0300

    контролле сатегорий

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -7,10 +7,40 @@ namespace controllers;
 class CategoryController 
 {
 	
-	 
+			 
+	//http://site.ru/    
 
 	public function actionIndex()
 	{
 		echo "cat index";
 	}
+
+
+	//http://site.ru/category/php 
+	public function actionArticleList($title = "")
+	{
+		 
+	}
+		//http://site.ru/article/php-article
+	public function actionArticleDetail($title = "")
+	{
+		 
+	}
+
+	public function add()
+	{
+		 
+	}
+
+	public function edite()
+	{
+		 
+	}
+
+	public function delete()
+	{
+		 
+	}
+
+
 }

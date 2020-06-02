8a0a560582ed211e6d91c8817ecfd309a0301e90
viktor <vk.job.00@mail.ru>
2020-06-02 15:47:38 +0300

    pages

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -13,14 +13,16 @@ class CategoryController extends FrontController
 	public function actionIndex()
 	{
 		 
-		
+		$this->view->setTmp('blog'); 
+		$this->view->content = $this->view->loadViewsPage("index");
+		$this->view->loadTemplate();
 	}
 
 
 	//http://site.ru/category/php 
 	public function actionArticleList($title = "")
-	{
-		 
+	{	
+
 	}
 		//http://site.ru/article/php-article
 	public function actionArticleDetail($title = "")
